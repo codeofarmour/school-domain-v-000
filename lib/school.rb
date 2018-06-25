e670a5ba3097f438c1e59041f5264d6f70fac5f0
Terri Riggle <codeofarmour@outlook.com>
2018-06-25 06:01:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,3 +11,13 @@ def add_student(name, grade)
   @roster[grade] ||= []
   @roster[grade] << name
 end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort 
+  @roster.each do |grade, students|
+    @roster[grade].sort!
+  end
+end
